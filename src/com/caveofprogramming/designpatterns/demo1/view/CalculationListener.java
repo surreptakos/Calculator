a7dbe42f9380df4654e17e337cd4647928f55b24
surreptakos <djgatsakos@gmail.com>
2015-04-28 21:05:02 -0500

    Revert "Calculator"
    This reverts commit 2dc791086bcdaa48437f47c0381765c990274c18.

--- src/com/caveofprogramming/designpatterns/demo1/view/CalculationListener.java
+++
@@ -1,5 +0,0 @@
-package com.caveofprogramming.designpatterns.demo1.view;
-
-public interface CalculationListener {
-	public void calculationPerformed(CalculationEvent event);
-}
--- src/com/caveofprogramming/designpatterns/demo1/view/OperationListener.java
+++
@@ -1,6 +0,0 @@
-package com.caveofprogramming.designpatterns.demo1.view;
-
-public interface OperationListener {
-	public void operationPerformed(OperationEvent event);
-
-}
